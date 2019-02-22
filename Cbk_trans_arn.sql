491b2dfbc4b382c25aeabbaf182e3db98bffc9da
Brezencat <46269810+Brezencat@users.noreply.github.com>
2019-02-22 17:22:46 +0300

    названия таблиц
    исправил названия БД и Таблиц

--- Cbk_trans_arn.sql
+++ Cbk_trans_arn.sql
@@ -1,19 +1,19 @@
 select  dc.acq_ref_number as ARN,     
         coalesce(dc2.ret_ref_number, dc.ret_ref_number) as RRN, /*coalesce смотрим, если в таблице dc2 поле RRN не заполнено, то заполняем его из таблицы dc*/
-        coalesce(bwx.rpr.get_tag_value(dc2.add_info,'RRN_IPSP'), (select rp.RRN_IPSP from reporter.accb_ext_data rp where bwx.rpr.get_tag_value(dc2.add_info,'ID_ACCB_EXT_DATA')=rp.id)) as RRN_IPSP,
+        coalesce(DB.rpr.get_tag_value(dc2.add_info,'RRN_IPSP'), (select rp.RRN_IPSP from report rp where DB.rpr.get_tag_value(dc2.add_info,'ID_ACCB_EXT_DATA')=rp.id)) as RRN_IPSP,
         SUBSTR(dc2.target_number,0,6)||'*****'||SUBSTR(dc2.target_number,13,4) as Karta,
         To_char(trunc(dc.trans_date,'dd'), 'dd.mm.yyyy') as Trans_Date, /*через to_char задаём нужный нам формат даты, trunc помогает, если формат не меняется.*/
         dc2.trans_amount as Amount,
-        (select currcode.name from bwx.currency currcode where currcode.amnd_state='A' and currcode.CODE=dc2.trans_curr) as Cur, /*обращаемся к таблице с кодами валют и заменяем значение кода на наименование валюты.*/
+        (select currcode.name from DB.currency currcode where currcode.amnd_state='A' and currcode.CODE=dc2.trans_curr) as Cur, /*обращаемся к таблице с кодами валют и заменяем значение кода на наименование валюты.*/
         dc.settl_amount as Cbk_Amount,
-        (select currcode.name from bwx.currency currcode where currcode.amnd_state='A' and currcode.CODE=dc.settl_curr)  as Cbk_curr, /*обращаемся к таблице с кодами валют и заменяем значение кода на наименование валюты.*/
+        (select currcode.name from DB.currency currcode where currcode.amnd_state='A' and currcode.CODE=dc.settl_curr)  as Cbk_curr, /*обращаемся к таблице с кодами валют и заменяем значение кода на наименование валюты.*/
         To_char(trunc(dc.posting_date,'dd'), 'dd.mm.yyyy') as Posting_Date, /*через to_char задаём нужный нам формат даты, trunc помогает, если формат не меняется.*/
-        (select trntype.name  trntype from bwx.trans_type trntype where trntype.amnd_state='A' and trntype.id=dc.trans_type) as Trans_type, /*обращаемся к таблицн с транс типом и меняем цифровой код на наименование.*/
+        (select trntype.name  trntype from DB.trans_type trntype where trntype.amnd_state='A' and trntype.id=dc.trans_type) as Trans_type, /*обращаемся к таблицн с транс типом и меняем цифровой код на наименование.*/
         case when substr(dc.reason_code,1,2)='VC' then substr(dc.reason_code,3) else dc.reason_code end) as reason_code, /* убираем из ризон кода Visa первые два символа, которые соответсвуют VC и оставляем всё остальное с третьего символа.*/
-        (select mesch.name from BWX.mess_channel mesch where mesch.amnd_state='A' and mesch.code=dc.source_channel) as System /* из таблицы каналов берем значение кода и вместо буквы выводим название канала.*/    
+        (select mesch.name from DB.mess_channel mesch where mesch.amnd_state='A' and mesch.code=dc.source_channel) as System /* из таблицы каналов берем значение кода и вместо буквы выводим название канала.*/    
 from
-        bwx.doc dc,
-        bwx.doc dc2
+        DB.table dc,
+        DB.table dc2
 where
         dc.amnd_state='A'
         and dc.acq_ref_number  in
