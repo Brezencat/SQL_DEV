aec453c762c02053dc978dc907b28d424af02aa8
Brezencat <46269810+Brezencat@users.noreply.github.com>
2019-02-23 23:37:31 +0300

    Update Exam70-461_solutions.sql
    решение на Задание 2

--- Exam70-461_solutions.sql
+++ Exam70-461_solutions.sql
@@ -1,7 +1,13 @@
-/*задание 1*/
+/*Задание 1*/
 Use TSQL2012
 
 select custid, YEAR(orderdate)
 from Sales.Orders
 order by 1, 2
+;
+
+/*Задание 2*/
+select DISTINCT custid, YEAR(orderdate) as Year_order
+from Sales.Orders
+--order by custid, Year_order
 ;
