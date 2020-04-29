54315c3e5fc5627ddbe312efa91b76c7ee0be40c
Brezencat <brezencat@gmail.com>
2020-04-29 22:58:25 +0300

    доработка процедуры

--- R&D/XEvent_log_viewer.sql
+++ R&D/XEvent_log_viewer.sql
@@ -1,4 +1,6 @@
 --create proc dbo.XEVENT_LOG_VIEWER
+SET XACT_ABORT ON;
+
 declare  @DATE varchar(11) = '2020-04-29'--null --строго в формате ГГГГ-ММ-ДД
 		,@XEVENT_NAME varchar(255) = null;
 --AS
@@ -20,21 +22,21 @@ CREATE TABLE #XEVENT_LIST
 IF @XEVENT_NAME is null
 BEGIN
 	INSERT INTO #XEVENT_LIST (ID, XEVENT_NAME, FILE_PATH)
-	select ROW_NUMBER() over (order by ID) as ID, EVENT_NAME, FILE_PATH
+	select ROW_NUMBER() over (order by ID) as ID, XEVENT_NAME, FILE_PATH
 	from dbo.XEVENT_LIST
 	where IS_ACTIVE = 1;
 END
 ELSE
 BEGIN
 	INSERT INTO #XEVENT_LIST (ID, XEVENT_NAME, FILE_PATH)
-	select CAST(1 as tinyint) as ID, EVENT_NAME, FILE_PATH
+	select CAST(1 as tinyint) as ID, XEVENT_NAME, FILE_PATH
 	from dbo.XEVENT_LIST
-	where EVENT_NAME = @XEVENT_NAME;
+	where XEVENT_NAME = @XEVENT_NAME;
 END
 
-drop table if exists #RESULT;
+drop table if exists #FILE_DATA;
 
-CREATE TABLE #RESULT
+CREATE TABLE #FILE_DATA
 	( XEVENT_NAME 	varchar(255)	NOT NULL --имя XEvent
 	 ,[EVENT]		varchar(255) 	NOT NULL --имя собоытия из XML
 	 ,[TIMESTAMP] 	datetime2 		NOT NULL --дата и время проишествия
@@ -43,14 +45,14 @@ CREATE TABLE #RESULT
 	 ,[APP_NAME] 	varchar(255) 	NOT NULL --client_app_name
 	 ,USERNAME 		varchar(255) 	NOT NULL 
 	 ,[SESSION_ID] 	smallint 		NOT NULL
-	 ,SQL_TEXT 		varchar(MAX) 	NULL --тексты запросов. exec процедуры
+	 ,SQL_TEXT 		varchar(4000) 	NULL --тексты запросов. exec процедуры
 
 	 ,session_id_query		int				null --с какими сессиями взаимодействие для lock_deadlock_chain
 	 ,database_id_query		tinyint			null --в каких базах эти сессии для lock_deadlock_chain
 	 ,deadlock_id			int				null --нужен для группировки событий
 	 ,transaction_id_query	int				null
 	 ,resource_type			varchar(10)		null --какой тип блокировки наложен
-	 ,DEADLOCK_GRAF			varchar(MAX)	null
+	-- ,DEADLOCK_GRAF			varchar(MAX)	null
 	 
 	 ,[VALUE] 		XML 			NULL --разные параметры
 	)
@@ -77,7 +79,7 @@ BEGIN
 		FROM sys.fn_xe_file_target_read_file (@FILE_PATH, null, null, null)
 		WHERE timestamp_utc like @DATE
 	)
-	INSERT INTO #RESULT
+	INSERT INTO #FILE_DATA
 		( XEVENT_NAME
 		 ,[EVENT]
 		 ,[TIMESTAMP]
@@ -92,7 +94,7 @@ BEGIN
 		 ,deadlock_id
 		 ,transaction_id_query
 		 ,resource_type			
-		 ,DEADLOCK_GRAF
+		 --,DEADLOCK_GRAF
 		 ,[VALUE]
 		)
 	SELECT  @SEANS_NAME as XEVENT_NAME
@@ -104,14 +106,14 @@ BEGIN
 			,data.value('(event/action[@name="client_app_name"]/value)[1]','varchar(255)') as [APP_NAME]
 			,data.value('(event/action[@name="username"]/value)[1]','varchar(255)') as USERNAME
 			,data.value('(event/action[@name="session_id"]/value)[1]','smallint') as SESSION_ID
-			,data.value('(event/action[@name="sql_text"]/value)[1]','varchar(MAX)') as SQL_TEXT
+			,data.value('(event/action[@name="sql_text"]/value)[1]','varchar(4000)') as SQL_TEXT
 			
 			,data.value('(event/data[@name="session_id"]/value)[1]','int') as session_id_query --с какими сессиями взаимодействие для lock_deadlock_chain
 			,data.value('(event/data[@name="database_id"]/value)[1]','tinyint') as database_id_query --в каких базах эти сессии для lock_deadlock_chain
 			,data.value('(event/data[@name="deadlock_id"]/value)[1]','int') as deadlock_id
 			,data.value('(event/data[@name="transaction_id"]/value)[1]','int') as transaction_id_query
 			,data.value('(event/data[@name="resource_type"]/text)[1]','varchar(10)') as resource_type --какой тип блокировки наложен
-			,data.value('(event/data[@name="xml_report"]/value)[1]','varchar(MAX)') as DEADLOCK_GRAF--это из xml_deadlock_report--вообе надо обрабатывать иначе
+			--,data.value('(event/data[@name="xml_report"]/value)[1]','varchar(MAX)') as DEADLOCK_GRAF--это из xml_deadlock_report--вообе надо обрабатывать иначе
 			,CASE WHEN data.value('(event/@name)[1]','varchar(255)') = 'xml_deadlock_report'
 				  THEN [DATA]
 				  ELSE NULL
@@ -121,47 +123,75 @@ BEGIN
 	set @i += 1
 END;
 
-select *--, COALESCE(DEADLOCK_GRAF,
-from #RESULT
-
---select XEVENT_NAME, MAX([TIMESTAMP]) as LASTDATE
---into #EVENT_DATE
+--select *--, COALESCE(DEADLOCK_GRAF,
 --from #RESULT
---GROUP BY XEVENT_NAME
---;
+--order by XEVENT_NAME, [TIMESTAMP]
 
---begin tran
+drop table if exists #RESULT;
+
+SELECT   d.XEVENT_NAME
+		,d.[EVENT]
+		,d.[TIMESTAMP]
+		,d.SERVER_NAME
+		,d.DATABASE_ID
+		,d.[APP_NAME]
+		,d.USERNAME
+		,d.SESSION_ID
+		,d.SQL_TEXT
+		,d.session_id_query
+		,d.database_id_query
+		,d.deadlock_id
+		,d.transaction_id_query
+		,d.resource_type
+		,d.[VALUE]
+into #RESULT
+FROM #FILE_DATA as d
+LEFT JOIN dbo.XEVENT_LOG as l on not exists (select d.XEVENT_NAME, d.[EVENT], d.[TIMESTAMP], d.SERVER_NAME, d.DATABASE_ID, d.[APP_NAME], d.USERNAME, d.SESSION_ID, d.session_id_query, d.database_id_query, d.deadlock_id, d.transaction_id_query, d.resource_type
+											 except
+											 select l.XEVENT_NAME, l.[EVENT], l.[TIMESTAMP], l.SERVER_NAME, l.DATABASE_ID, l.[APP_NAME], l.USERNAME, l.SESSION_ID, l.session_id_query, l.database_id_query, l.deadlock_id, l.transaction_id_query, l.resource_type)
+WHERE l.XEVENT_NAME is null;
+
+
+begin tran
 	
---	INSERT INTO dbo.XEVENT_LOG
---		( XEVENT_NAME
---		 ,[EVENT]
---		 ,[TIMESTAMP]
---		 ,SERVER_NAME
---		 ,DATABASE_ID
---		 ,[APP_NAME]
---		 ,USERNAME
---		 ,[SESSION_ID]
---		 ,SQL_TEXT
-		
---		 ,[VALUE]
---		)
---	SELECT  XEVENT_NAME
---			,[EVENT]
---			,[TIMESTAMP] 
---			,SERVER_NAME 
---			,DATABASE_ID 
---			,[APP_NAME] 
---			,USERNAME 
---			,[SESSION_ID] 
---			,SQL_TEXT 
-			
---			,[VALUE]
---	FROM #RESULT
+	INSERT INTO dbo.XEVENT_LOG
+		( XEVENT_NAME
+		 ,[EVENT]
+		 ,[TIMESTAMP]
+		 ,SERVER_NAME
+		 ,DATABASE_ID
+		 ,[APP_NAME]
+		 ,USERNAME
+		 ,SESSION_ID
+		 ,SQL_TEXT
+		 ,session_id_query
+		 ,database_id_query
+		 ,deadlock_id
+		 ,transaction_id_query
+		 ,resource_type
+		 ,[VALUE]
+		)
+	SELECT   XEVENT_NAME
+			,[EVENT]
+			,[TIMESTAMP]
+			,SERVER_NAME
+			,DATABASE_ID
+			,[APP_NAME]
+			,USERNAME
+			,SESSION_ID
+			,SQL_TEXT
+			,session_id_query
+			,database_id_query
+			,deadlock_id
+			,transaction_id_query
+			,resource_type
+			,[VALUE]
+	FROM #RESULT;
+	
 
+	update el
+	set el.LAST_DATE = getdate()
+	from dbo.XEVENT_LIST as el
+	inner join #XEVENT_LIST as d ON el.XEVENT_NAME = d.XEVENT_NAME;
 
---	update el
---	set el.LASTDATE = d.LASTDATE
---	from dbo.XEVENT_LIST as el
---	inner join #EVENT_DATE as d ON el.EVENT_NAME = d.EVENT_NAME;
-
---commit
+commit
