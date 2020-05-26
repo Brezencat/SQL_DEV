cf75a282a235042129773d92dca0a38c7dab96fb
Brezencat <brezencat@gmail.com>
2020-05-26 10:30:56 +0300

    финальная доработка эвентов

--- R&D/XEvent_LOGIN_APP_CONNECT.sql
+++ R&D/XEvent_LOGIN_APP_CONNECT.sql
@@ -1,35 +1,33 @@
 CREATE EVENT SESSION [LOGIN_APP_CONNECT] ON SERVER 
-ADD EVENT sqlserver.login
-	(
-	 SET collect_database_name=(1)
+ADD EVENT sqlserver.login (
+	SET collect_database_name=(1)
 		,collect_options_text=(1)
-     ACTION ( sqlserver.client_app_name
-			 ,sqlserver.client_hostname
-			 ,sqlserver.session_id
-			 ,sqlserver.username
-		    )
-     WHERE ([sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[username],N'NT SERVICE\SQLSERVERAGENT') 
-		AND [sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[username],N'NT Service\SSISScaleOutMaster140') 
-		AND [sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[username],N'NT SERVICE\SQLTELEMETRY') 
-		AND [sqlserver].[not_equal_i_sql_unicode_string]([sqlserver].[client_app_name],N'.Net SqlClient Data Provider') 
-		AND NOT [sqlserver].[like_i_sql_unicode_string]([sqlserver].[client_app_name],N'%Microsoft SQL Server Management Studio%') 
-		AND NOT [sqlserver].[like_i_sql_unicode_string]([sqlserver].[client_app_name],N'%Service Broker%')
-		   )
-	)
-ADD TARGET package0.event_file (SET 
-									 filename=N'LOGIN_APP_CONNECT'
-									,max_file_size=(10)
-									,max_rollover_files=(0)
-							   )
-WITH (
-		 MAX_MEMORY=4096 KB
-		,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS
-		,MAX_DISPATCH_LATENCY=3 SECONDS
-		,MAX_EVENT_SIZE=0 KB
-		,MEMORY_PARTITION_MODE=NONE
-		,TRACK_CAUSALITY=OFF
-		,STARTUP_STATE=ON
-	 )
+    ACTION ( sqlserver.client_app_name
+			,sqlserver.client_hostname
+			,sqlserver.nt_username
+			,sqlserver.session_id
+			,sqlserver.sql_text
+			,sqlserver.username)
+    WHERE ( [sqlserver].[not_equal_i_sql_unicode_string] ([sqlserver].[username],N'NT SERVICE\SQLSERVERAGENT') 
+		AND [sqlserver].[not_equal_i_sql_unicode_string] ([sqlserver].[username],N'NT Service\SSISScaleOutMaster140') 
+		AND [sqlserver].[not_equal_i_sql_unicode_string] ([sqlserver].[username],N'NT SERVICE\SQLTELEMETRY') 
+		AND [sqlserver].[not_equal_i_sql_unicode_string] ([sqlserver].[client_app_name],N'.Net SqlClient Data Provider') 
+		AND NOT [sqlserver].[like_i_sql_unicode_string] ([sqlserver].[client_app_name],N'%Microsoft SQL Server Management Studio%') 
+		AND NOT [sqlserver].[like_i_sql_unicode_string] ([sqlserver].[client_app_name],N'%Service Broker%')))
+ADD TARGET package0.event_file ( 
+	SET filename=N'C:\Users\MSSQLSERVER\Documents\XEVENT_LOG\LOGIN_APP_CONNECT.xel'
+				,max_file_size=(300)
+				,max_rollover_files=(3))
+
+WITH ( MAX_MEMORY=4096 KB
+	  ,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS --возможная потеря одного события, если выделенный буфер памяти для события будет заполнен (при записи данных события на диск из памяти)
+	  ,MAX_DISPATCH_LATENCY=3 SECONDS --задержка информации о событии в буферной памяти, после которой выполняется запись на диск (влияет на заполнение буфера)
+	  ,MAX_EVENT_SIZE=0 KB
+	  ,MEMORY_PARTITION_MODE=NONE
+	  ,TRACK_CAUSALITY=OFF
+	  ,STARTUP_STATE=ON) --запуск эвента со стартом сервера
 GO
 
-
+--запуск эвента после создания
+ALTER EVENT SESSION [DEADLOCK_MONITOR] ON SERVER
+	STATE = START;
--- R&D/XEvetns_DEADLOCK_MONITOR.sql
+++ R&D/XEvetns_DEADLOCK_MONITOR.sql
@@ -1,51 +1,26 @@
 CREATE EVENT SESSION [DEADLOCK_MONITOR] ON SERVER 
---https://docs.microsoft.com/ru-ru/sql/relational-databases/event-classes/lock-deadlock-event-class?view=sql-server-2017
-ADD EVENT sqlserver.lock_deadlock -- событие lock_deadlock отслеживает возникновение взаимоблокировок и объекты, которые в них участвуют
-	(
-    ACTION ( sqlserver.client_app_name
+ADD EVENT sqlserver.xml_deadlock_report ( --отчет о взаимоблокировке в формате XML
+	ACTION ( sqlserver.client_app_name
 			,sqlserver.client_hostname
 			,sqlserver.database_id
+			,sqlserver.nt_username
 			,sqlserver.server_principal_name
 			,sqlserver.session_id
 			,sqlserver.sql_text
-			,sqlserver.transaction_id
-			,sqlserver.username
-		   )
-	),
---https://docs.microsoft.com/ru-ru/sql/relational-databases/event-classes/lock-deadlock-chain-event-class?view=sql-server-2017
-ADD EVENT sqlserver.lock_deadlock_chain -- событие lock_deadlock_chain регистрирует условие возникновения взаимоблокировок
-
-	(
-    ACTION ( sqlserver.client_app_name
-			,sqlserver.client_hostname
-			,sqlserver.database_id
-			,sqlserver.server_principal_name
-			,sqlserver.session_id
-			,sqlserver.sql_text
-			,sqlserver.transaction_id
-			,sqlserver.username
-		   )
-	),
-ADD EVENT sqlserver.xml_deadlock_report --отчет о взаимоблокировке в формате XML
-	(
-    ACTION ( sqlserver.client_app_name
-			,sqlserver.client_hostname
-			,sqlserver.database_id
-			,sqlserver.server_principal_name
-			,sqlserver.session_id
-			,sqlserver.sql_text
-			,sqlserver.transaction_id
-			,sqlserver.username
-		   )
-	)
-ADD TARGET package0.event_file (SET 
-									 filename = N'DEADLOCK_MONITOR'
-									,max_file_size = (10) --размер файла лога в Мб
-									,max_rollover_files = (0) -- количество файлов лога (если 0, то перезапись лога старый файл)
-							   ) 
-WITH (
-		STARTUP_STATE=ON
-	 )
+			,sqlserver.username))
+ADD TARGET package0.event_file ( 
+	SET filename=N'C:\Users\MSSQLSERVER\Documents\XEVENT_LOG\DEADLOCK_MONITOR.xel'
+				,max_file_size=(100)  --размер файла лога в Мб
+				,max_rollover_files = (5)) --количество файлов лога (если 0, то перезапись лога в один файл)
+WITH ( MAX_MEMORY=4096 KB
+	  ,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS --возможная потеря одного события, если выделенный буфер памяти для события будет заполнен (при записи данных события на диск из памяти)
+	  ,MAX_DISPATCH_LATENCY=3 SECONDS --задержка информации о событии в буферной памяти, после которой выполняется запись на диск (влияет на заполнение буфера)
+	  ,MAX_EVENT_SIZE=0 KB
+	  ,MEMORY_PARTITION_MODE=NONE
+	  ,TRACK_CAUSALITY=OFF
+	  ,STARTUP_STATE=ON) --запуск эвента со стартом сервера
 GO
 
-
+--запуск эвента после создания
+ALTER EVENT SESSION [DEADLOCK_MONITOR] ON SERVER
+	STATE = START;
