4f185646826e3a204e7e4139534c34d003f3c5dd
Brezencat <brezencat@gmail.com>
2021-06-23 15:59:03 +0300

    добавил ещё пример рекурсии

--- EDUCATION/CTE рекурсия.sql
+++ EDUCATION/CTE рекурсия.sql
@@ -16,12 +16,27 @@ FROM (
 ;
 
 WITH CTE 
-     AS (         select ID, PRNAME from PRICE where ID=1         union all         select ID+1, PRNAME from cte as c where c.id<=3
-        ) SELECT * FROM CTE
+     AS (
+         select ID, PRNAME from PRICE where ID=1
+         union all
+         select ID+1, PRNAME from cte as c where c.id<=3
+        ) 
+SELECT * FROM CTE
 ;
 --Результат: 
 /*
-1	AA1	AB1	AC2	AC3	AC4	AC2	AB3	AB4	AB2	AA3	AA4	AA
+1	AA
+1	AB
+1	AC
+2	AC
+3	AC
+4	AC
+2	AB
+3	AB
+4	AB
+2	AA
+3	AA
+4	AA
 */
 
 
@@ -39,3 +54,60 @@ WITH CTE
          select id+1, num+1 from cte where id < 5
         )
 SELECT * FROM CTE
+
+--===============================================================================
+
+declare @t1 table
+	(id tinyint identity(1,1)
+	,[name] char(1)
+	,parent_id tinyint
+	)
+;
+
+insert into @t1
+	([name], parent_id)
+values
+	 ('a', 0)
+	,('b', 1)
+	,('c', 2)
+	,('d', 3)
+	,('e', 1)
+	,('f', 2)
+	,('g', 2)
+;
+
+--select *
+--from @t1
+--;
+
+
+--select t.*, p.name
+--from @t1 as t
+--left join @t1 as p
+--	on p.id = t.parent_id
+--;
+
+WITH CTE AS
+(
+	select  id
+		,	[name]
+		,	parent_id
+		,	CAST('' as char(1)) as parent_name
+		,	CAST(0 as int) as [level]
+	from @t1
+	where parent_id = 0
+	UNION ALL
+	select  t.id
+		,	t.[name]
+		,	t.parent_id
+		,	c.[name]
+		,	c.[level] + 1
+	from CTE as c
+	inner join @t1 as t
+		on c.id = t.parent_id
+)
+select *
+from CTE
+;
+
+--===============================================================================
